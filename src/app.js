e8ba4f60ff17fd9431b0dfe9d8de146cd191d128
Paula Vitoria <paulavitoriabrasil@gmail.com>
2024-06-04 21:44:21 -0300

    rota put - buscar livros por id

--- src/app.js
+++ src/app.js
@@ -38,6 +38,12 @@ app.post("/livros", (req, res) => {
     res.status(201).send("livro cadastrado com sucesso")
 });
 
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id)
+    livros[index].titulo = req.body.titulo
+    res.status(200).json(livros)
+})
+
 
 
 export default app;
