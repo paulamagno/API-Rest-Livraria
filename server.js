931d325f01dea38384f49c1ebd3caa0bc94fb61a
Paula Vitoria <paulavitoriabrasil@gmail.com>
2024-06-04 21:00:52 -0300

    rotas get e post

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Curso de Node.js"
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
-    console.log("Servidor escutando!")
+app.listen(PORT, () => {
+    console.log("Servidor escutando!")   
 });
 
---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+
+const app = express();
+app.use(express.json())
+
+const livros = [
+    {
+        id:1,
+        titulo: "O Senhor dos Anéis"
+    },
+    {
+        id: 2,
+        titulo: "O Hobbit"
+    }
+]
+
+app.get("/", (req, res) => {
+    res.status(200).send('Curso de Node.js')
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("livro cadastrado com sucesso")
+});
+
+
+
+export default app;
